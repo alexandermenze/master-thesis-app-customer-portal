622a60c0a7f429cc0a3cc57f809a8931502e6247
Stefan Mehnert <s.mehnert@polipol.de>
2025-06-02 11:17:55 +0200

    Implemented new Feature Reporting-Service, Changed Threat-Modelling

--- src/CustomerPortal.ReportingService/BackgroundServices/UsersReportBackgroundService.cs
+++ src/CustomerPortal.ReportingService/BackgroundServices/UsersReportBackgroundService.cs
@@ -1,9 +1,26 @@
-﻿namespace CustomerPortal.ReportingService.BackgroundServices;
+﻿using CustomerPortal.Messages.Dtos;
+
+namespace CustomerPortal.ReportingService.BackgroundServices;
 
 public class UsersReportBackgroundService : BackgroundService
 {
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        throw new NotImplementedException();
+        var userCount = await LoadUserCountFromApi();
+        await StoreUserStatistics(DateTime.Now, userCount);
+    }
+
+    private async Task<int> LoadUserCountFromApi()
+    {
+        var httpClient = new HttpClient();
+        httpClient.BaseAddress = new Uri("http://localhost:5000/api/users");
+        var users =  await httpClient.GetFromJsonAsync<IEnumerable<UserResponseDto>>("");
+        return users?.Count() ?? 0;
+    }
+
+    private async Task StoreUserStatistics(DateTime timestamp, int userCount)
+    {
+        var record = $"{timestamp:O};{userCount}";
+        await File.AppendAllTextAsync(@"c:\user-stats.csv", record);
     }
 }
--- src/CustomerPortal.ReportingService/Controllers/ReportController.cs
+++ src/CustomerPortal.ReportingService/Controllers/ReportController.cs
@@ -6,8 +6,31 @@ namespace CustomerPortal.ReportingService.Controllers;
 [Route("reports")]
 public class ReportController : ControllerBase
 {
-    public Task<IActionResult> GetUsersReport()
+    public async Task<IActionResult> GetUsersReport()
     {
-        throw new NotImplementedException();
+        var userStatisticsCsv = await GetUserStatisticsFromFile();
+        var userStatisticsResponse = UserStatisticsAsResponse(userStatisticsCsv);
+        return Ok(userStatisticsResponse);
+    }
+
+    private async Task<IEnumerable<string>> GetUserStatisticsFromFile()
+    {
+        var userStatisticLines = await System.IO.File.ReadAllLinesAsync(@"c:\user-stats.csv");
+        return userStatisticLines;
+    }
+    
+    private IEnumerable<UserStatisticsResponse> UserStatisticsAsResponse(IEnumerable<string> userStatisticsCsv)
+    {
+        return userStatisticsCsv.Select(u => new UserStatisticsResponse
+        {
+            DateTime = DateTime.Parse(u.Split(",")[0]),
+            UserCount = int.Parse(u.Split(",")[1])
+        });
+    }
+    
+    private record UserStatisticsResponse
+    {
+        public DateTime DateTime { get; set; }
+        public int UserCount { get; set; }
     }
 }
